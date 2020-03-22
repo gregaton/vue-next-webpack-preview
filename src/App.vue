3885dae52030361b9aad19000bddcb7a36e8f75d
Gregor Heinze <gregor.heinze@gmail.com>
2020-03-22 13:00:24 +0100

    Added postcss-loader and tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <img src="./logo.png">
-  <h1>Hello Vue 3!</h1>
-  <button @click="inc">Clicked {{ count }} times.</button>
+  <div class="container mx-auto">
+    <img src="./logo.png">
+    <h1>Hello Vue 3!</h1>
+    <button @click="inc" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Clicked {{ count }} times.
+    </button>
+  </div>
 </template>
 
 <script>
--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,8 @@ module.exports = (env = {}) => ({
             loader: MiniCssExtractPlugin.loader,
             options: { hmr: !env.prod }
           },
-          'css-loader'
+          'css-loader',
+          'postcss-loader'
         ]
       }
     ]
